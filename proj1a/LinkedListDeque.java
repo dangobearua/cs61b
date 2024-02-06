6a1ee49a771d3c2b26a3f7ef34d2e10f9b252c5a
Ayan Barua <ayanbarua@Ayans-MacBook-Pro.local>
2024-02-05 17:22:27 -0800

    finished proj1a

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,95 @@
+public class LinkedListDeque<T> {
+
+    public class Node {
+        public Node prev;
+        public T item;
+        public Node next;
+
+        public Node(Node prev, T item, Node next) {
+            this.prev = prev;
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    private Node sentinel;
+    private int size;
+
+    public LinkedListDeque() {
+        sentinel = new Node(null, null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
+    }
+
+    public void addFirst(T item) {
+        Node newNode = new Node(sentinel, item, sentinel.next);
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        Node newNode = new Node(sentinel.prev, item, sentinel);
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
+        size += 1;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        Node p = sentinel.next;
+
+        while (p != sentinel) {
+            if (p.next == sentinel) {
+                System.out.println(p.item);
+                break;
+            }
+            System.out.print(p.item + " ");
+            p = p.next;
+        }
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        T first = sentinel.next.item;
+        sentinel.next.next.prev = sentinel;
+        sentinel.next = sentinel.next.next;
+        size -= 1;
+        return first;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        T last = sentinel.prev.item;
+        sentinel.prev.prev.next = sentinel;
+        sentinel.prev = sentinel.prev.prev;
+        size -= 1;
+        return last;
+    }
+
+    public T get(int index) {
+        if (index >= size || size == 0) {
+            return null;
+        }
+        Node p = sentinel;
+        int count = 0;
+        while (count < index) {
+            count += 1;
+            p = p.next;
+        }
+        return p.item;
+    }
+
+}
